d3dae407b1447a4a54c9d35c266b5603395f5299
johnryzon123 <junilracho123@gmail.com>
2025-03-20 20:04:23 +0800

    main.js

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-document.getElementById('playf').addEventListener('click', function() {
-    // Navigate to sceneA.html
+// Define the functions first
+function playf() {
+    // Code to navigate to Chess.html
     window.location.href = 'Chess.html';
-});
+}
 
-document.getElementById('playb').addEventListener('click', function() {
-    // Navigate to sceneB.html
-    window.location.href = 'Chessb.html';
-});
+function playb() {
+    // Code to navigate to sceneB.html
+    window.location.href = 'sceneB.html';
+}
+
+// Attach the functions to button click events
+document.getElementById('chessf').addEventListener('click', playf);
+document.getElementById('chessb').addEventListener('click', playb);
